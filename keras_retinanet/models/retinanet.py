b4f5fd9ac93dd2e5d3f083a7cf5a8a170a09059c
Dmytro S Lituiev <d.lituiev@gmail.com>
2018-08-06 20:14:17 +0000

    fixes

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -26,7 +26,7 @@ def default_classification_model(
     num_anchors,
     pyramid_feature_size=256,
     prior_probability=0.01,
-    classification_feature_size=[256]*4,
+    feature_sizes=[256]*4,
     name='classification_submodel'
     ):
     """ Creates the default regression submodel.
@@ -202,7 +202,6 @@ class ClassModel(keras.models.Model):
                 num_anchors,
                 pyramid_feature_size=256,
                 prior_probability=0.01,
-                classification_feature_size=256,
                 feature_sizes=[256]*2, 
                 name='classification_submodel',
                 activation='relu',
@@ -399,8 +398,7 @@ def __build_anchors(anchor_parameters, features):
     return keras.layers.Concatenate(axis=1, name='anchors')(anchors)
 
 
-class RetinaNet(keras.models.Model):
-    def __init__(self,
+def RetinaNet(
         inputs,
         backbone_layers,
         num_classes,
@@ -410,7 +408,8 @@ class RetinaNet(keras.models.Model):
         name                    = 'retinanet',
         share_rpn               = True,
         class_feature_sizes=[256]*4,
-        regr_feature_sizes=[256]*4)
+        regr_feature_sizes=[256]*4,
+        common_feature_sizes=[256]*4,
     ):
         """ Construct a RetinaNet model on top of a backbone.
 
@@ -441,57 +440,57 @@ class RetinaNet(keras.models.Model):
         elif submodels == 'joint_submodels':
             submodels = joint_submodels(num_classes, num_anchors,
                                         class_feature_sizes=class_feature_sizes,
-                                        regr_feature_sizes=regr_feature_sizes)
+                                        regr_feature_sizes=regr_feature_sizes,
+                                        common_feature_sizes=common_feature_sizes,)
 
         C3, C4, C5 = backbone_layers
 
         # compute pyramid features as per https://arxiv.org/abs/1708.02002
         if create_pyramid_features is None:
-            create_pyramid_features = self._create_pyramid_features
+            create_pyramid_features = _create_pyramid_features
         features = create_pyramid_features(C3, C4, C5)
 
         # for all pyramid levels, run available submodels
         pyramids = _build_pyramid(submodels, features, share=share_rpn)
-        super(RetinaNet, self).__init__(inputs=inputs, outputs=pyramids, name=name)
-        #return keras.models.Model(inputs=inputs, outputs=pyramids, name=name)
+        #super(RetinaNet, self).__init__(inputs=inputs, outputs=pyramids, name=name)
+        return keras.models.Model(inputs=inputs, outputs=pyramids, name=name)
 
-    @classmethod
-    def _create_pyramid_features(cls, C3, C4, C5, feature_size=256):
-        """ Creates the FPN layers on top of the backbone features.
+def _create_pyramid_features(C3, C4, C5, feature_size=256):
+    """ Creates the FPN layers on top of the backbone features.
 
-        Args
-            C3           : Feature stage C3 from the backbone.
-            C4           : Feature stage C4 from the backbone.
-            C5           : Feature stage C5 from the backbone.
-            feature_size : The feature size to use for the resulting feature levels.
+    Args
+        C3           : Feature stage C3 from the backbone.
+        C4           : Feature stage C4 from the backbone.
+        C5           : Feature stage C5 from the backbone.
+        feature_size : The feature size to use for the resulting feature levels.
 
-        Returns
-            A list of feature levels [P3, P4, P5, P6, P7].
-        """
-        # upsample C5 to get P5 from the FPN paper
-        P5           = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C5_reduced')(C5)
-        P5_upsampled = layers.UpsampleLike(name='P5_upsampled')([P5, C4])
-        P5           = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P5')(P5)
+    Returns
+        A list of feature levels [P3, P4, P5, P6, P7].
+    """
+    # upsample C5 to get P5 from the FPN paper
+    P5           = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C5_reduced')(C5)
+    P5_upsampled = layers.UpsampleLike(name='P5_upsampled')([P5, C4])
+    P5           = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P5')(P5)
 
-        # add P5 elementwise to C4
-        P4           = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C4_reduced')(C4)
-        P4           = keras.layers.Add(name='P4_merged')([P5_upsampled, P4])
-        P4_upsampled = layers.UpsampleLike(name='P4_upsampled')([P4, C3])
-        P4           = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P4')(P4)
+    # add P5 elementwise to C4
+    P4           = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C4_reduced')(C4)
+    P4           = keras.layers.Add(name='P4_merged')([P5_upsampled, P4])
+    P4_upsampled = layers.UpsampleLike(name='P4_upsampled')([P4, C3])
+    P4           = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P4')(P4)
 
-        # add P4 elementwise to C3
-        P3 = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C3_reduced')(C3)
-        P3 = keras.layers.Add(name='P3_merged')([P4_upsampled, P3])
-        P3 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P3')(P3)
+    # add P4 elementwise to C3
+    P3 = keras.layers.Conv2D(feature_size, kernel_size=1, strides=1, padding='same', name='C3_reduced')(C3)
+    P3 = keras.layers.Add(name='P3_merged')([P4_upsampled, P3])
+    P3 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=1, padding='same', name='P3')(P3)
 
-        # "P6 is obtained via a 3x3 stride-2 conv on C5"
-        P6 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P6')(C5)
+    # "P6 is obtained via a 3x3 stride-2 conv on C5"
+    P6 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P6')(C5)
 
-        # "P7 is computed by applying ReLU followed by a 3x3 stride-2 conv on P6"
-        P7 = keras.layers.Activation('relu', name='C6_relu')(P6)
-        P7 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P7')(P7)
+    # "P7 is computed by applying ReLU followed by a 3x3 stride-2 conv on P6"
+    P7 = keras.layers.Activation('relu', name='C6_relu')(P6)
+    P7 = keras.layers.Conv2D(feature_size, kernel_size=3, strides=2, padding='same', name='P7')(P7)
 
-        return [P3, P4, P5, P6, P7]
+    return [P3, P4, P5, P6, P7]
 
 
 def retinanet_bbox(
